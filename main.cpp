d48e8ab1284940257e21f8866925854f7cb45594
vlad <maivlad18@gmail.com>
2023-08-30 00:47:26 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+char generateRandomChar() {
+    const std::string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return characters[rand() % characters.length()];
+}
+
+// Function to generate a serial key
+std::string generateSerialKey(int length) {
+    std::string serialKey;
+    for (int i = 0; i < length; ++i) {
+        serialKey += generateRandomChar();
+    }
+    return serialKey;
+}
+
+std::string insertDashes(const std::string& word) {
+    std::string result;
+    for (size_t i = 0; i < word.length(); i++) {
+        result += word[i];
+        if ((i + 1) % 4 == 0 && i != word.length() - 1) {
+            result += '-';
+        }
+    }
+    return result;
+}
+
+int main() {
+    while(true){
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    int keyLength;
+    std::cout<<"Enter the length of the Product key: "; std::cin>>keyLength;
+    std::string serialKey = generateSerialKey(keyLength);
+
+    std::cout << "Generated Serial Key: " << serialKey <<'\n';
+std::string dashedkey = insertDashes(serialKey);
+    std::cout << "Product key with dashes: " << dashedkey << '\n'<< '\n'<< '\n';
+
+    }
+   return 0;
+}
